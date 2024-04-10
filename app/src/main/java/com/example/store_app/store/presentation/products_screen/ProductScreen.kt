ee9ea01856c6061b57079f48950faf70a56dc536
Wesley de Oliveira <wesleysistemaor@gmail.com>
2024-04-10 18:27:27 -0300

    STORE_APP -PRODUCTSSCREEN -LOADINGDIALOG -MYTOPAPPBAR

--- app/src/main/java/com/example/store_app/store/presentation/products_screen/ProductScreen.kt
+++ app/src/main/java/com/example/store_app/store/presentation/products_screen/ProductScreen.kt
@@ -1,6 +1,9 @@
 package com.example.store_app.store.presentation.products_screen
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import com.example.store_app.store.presentation.ultil.LoadingDialog
 
 @Composable
 internal fun ProductsScreen(){
@@ -12,5 +15,11 @@ fun ProductsContext(
     state: ProductsViewState
 ){
 
+LoadingDialog(isLoading = state.isLoading)
+    Scaffold(  modifier = Modifier.fillMaxWidth(),
+        topBar ={
 
+        } ) {
+
+    }
 }
