87ab81978446a77f16df0d0ecacc1e2224099701
Wesley de Oliveira <wesleysistemaor@gmail.com>
2024-04-16 17:46:12 -0300

    Store_App implementation ProductScreen.kt

--- app/src/main/java/com/example/store_app/store/presentation/products_screen/ProductScreen.kt
+++ app/src/main/java/com/example/store_app/store/presentation/products_screen/ProductScreen.kt
@@ -1,9 +1,14 @@
 package com.example.store_app.store.presentation.products_screen
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.store_app.store.presentation.ultil.LoadingDialog
+import com.example.store_app.store.presentation.ultil.MyTopAppBar
 
 @Composable
 internal fun ProductsScreen(){
@@ -18,8 +23,13 @@ fun ProductsContext(
 LoadingDialog(isLoading = state.isLoading)
     Scaffold(  modifier = Modifier.fillMaxWidth(),
         topBar ={
+           MyTopAppBar(title = "Products")
+        }
+    ) {
+LazyVerticalStaggeredGrid(modifier = Modifier.padding(top = it.calculateTopPadding()) ,
+    columns = StaggeredGridCells.Fixed(2)
+){
 
-        } ) {
-// so comentario:
+}
     }
 }
