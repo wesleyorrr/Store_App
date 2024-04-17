00a42f19e04fb3d9e483135117768c47cbe0617d
Wesley de Oliveira <wesleysistemaor@gmail.com>
2024-04-17 16:25:38 -0300

    Store_App
    ProductCard implementation

--- app/src/main/java/com/example/store_app/store/presentation/products_screen/components/ProductCard.kt
+++ app/src/main/java/com/example/store_app/store/presentation/products_screen/components/ProductCard.kt
@@ -1,5 +1,11 @@
 package com.example.store_app.store.presentation.products_screen.components
 
+import android.widget.Space
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -7,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.example.store_app.store.domain.model.Product
 
 @Composable
@@ -21,7 +28,19 @@ fun ProductCard(
         colors = CardDefaults.cardColors(contentColor = Color.White)
 
     ){
+        Column(modifier = Modifier.padding(15.dp)) {
+            AsyncImage(model = product.image,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1f),
+                constentSpace = ContextScale.FillBounds
+            )
+            Space(modifier = Modifier.height(5.dp))
 
+
+
+        }
     }
 
 }
