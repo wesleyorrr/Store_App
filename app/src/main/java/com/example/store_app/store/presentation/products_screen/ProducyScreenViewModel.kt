2fb98471bfce886fec43f6333538f0f9d6084285
Wesley de Oliveira <wesleysistemaor@gmail.com>
2024-04-19 15:38:09 -0300

    Store_App
    Create ProducyScreenViewModel

---
+++ app/src/main/java/com/example/store_app/store/presentation/products_screen/ProducyScreenViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.store_app.store.presentation.products_screen
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ProducyScreenViewModel @Inject constructor():ViewModel() {
+}
