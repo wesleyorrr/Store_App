e7fffb06e41a8a8ef40bb942299b3736167abb9e
Wesley de Oliveira <wesleysistemaor@gmail.com>
2024-04-19 15:44:18 -0300

    Store_App
    alt ProducyScreenViewModel

--- app/src/main/java/com/example/store_app/store/presentation/products_screen/ProducyScreenViewModel.kt
+++ app/src/main/java/com/example/store_app/store/presentation/products_screen/ProducyScreenViewModel.kt
@@ -1,9 +1,20 @@
 package com.example.store_app.store.presentation.products_screen
 
 import androidx.lifecycle.ViewModel
+import com.example.store_app.store.domain.repository.ProductsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class ProducyScreenViewModel @Inject constructor():ViewModel() {
+class ProducyScreenViewModel @Inject constructor(
+    private val productsRepository:ProductsRepository
+):ViewModel() {
+    private val _state = MutableStateFlow(ProductsViewState())
+    val state = _state.asStateFlow()
+
+    fun getProducts(){
+
+    }
 }
