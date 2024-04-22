908ce8bf1cde7bfcf3d9eaf91d20f48b65b06cca
Wesley de Oliveira <wesleysistemaor@gmail.com>
2024-04-22 16:32:34 -0300

    Store_App
    alt Project2

--- app/src/main/java/com/example/store_app/util/EventBus.kt
+++ app/src/main/java/com/example/store_app/util/EventBus.kt
@@ -1,4 +1,19 @@
 package com.example.store_app.util
 
- object EventBus {
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+
+object EventBus {
+     private val _events = Channel<Any>()
+    val events = _events.receiveAsFlow()
+
+    suspend fun sendEvent(event : Any){
+        _events.send(event)
+    }
+}
+
+sealed interface Event{
+    data class Toast(val menssage: String): Event
+    //data class Dialg(val title:String, val text : String): Event
+
 }
